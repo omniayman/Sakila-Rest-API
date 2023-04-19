6eef5d95a597fb6e23c6fb0f52b9ba97889deb02
omniayman <58345026+omniayman@users.noreply.github.com>
2023-04-19 08:55:00 +0200

    Update Category.java

--- src/main/java/org/example/presentation/controllers/Category.java
+++ src/main/java/org/example/presentation/controllers/Category.java
@@ -1,6 +1,10 @@
 package org.example.presentation.controllers;
 
 
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
+import org.example.dtos.StaffDto;
+import org.example.dtos.address.AddressDto;
 import org.example.dtos.category.CategoryDto;
 import org.example.dtos.film.FilmDto;
 import org.example.presistance.exceptionHandler.InvalidDataException;
@@ -8,39 +12,68 @@ import org.example.services.CategoryService;
 
 import java.util.List;
 
-
+@Path("category")
 public class Category {
     CategoryService categoryService = new CategoryService();
 
-    public List<CategoryDto> getAllCategories() {
-        return categoryService.getAllCategories();
+    @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response getAllCategories(@Context UriInfo uriInfo) {
+        List<CategoryDto> categories = categoryService.getAllCategories();
+        GenericEntity entity = new GenericEntity<List<CategoryDto>>(categories) {
+        };
+        Link self = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+        return Response.ok(entity).links(self).build();
+    }
+    @GET
+    @Path("{oid}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response getCategoryById(@PathParam("oid") int id, @Context UriInfo uriInfo) {
+        Link self = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+        Link getAll = Link.fromPath(uriInfo.getBaseUri().toString().concat("address")).build();
+        CategoryDto category = categoryService.getCategoryById(id);
+        return Response.ok(category).link(self.getUri(), "self").link(getAll.getUri(), "getAll").build();
     }
 
-    public CategoryDto getCategoryById(int id) {
-        return categoryService.getCategoryById(id);
-    }
-
+    @PUT
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public void updateCategory(CategoryDto categoryDto) {
 
         categoryService.updateCategory(categoryDto);
     }
 
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public void addCategory(CategoryDto categoryDto) throws InvalidDataException {
         categoryService.addCategory(categoryDto);
     }
 
-    public List<FilmDto> getFilmsByCategoryId(int id) {
+    @GET
+    @Path("{id}/films")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response getFilmsByCategoryId(@PathParam("id")int id) {
 
-        return categoryService.getFilmsByCategoryId(id);
+        List<FilmDto> filmDtos = categoryService.getFilmsByCategoryId(id);
+        GenericEntity entity = new GenericEntity<List<FilmDto>>(filmDtos) {
+        };
+        return Response.ok(entity).build();
 
     }
 
-    public List<CategoryDto> getCategoryByName(String name) {
+    @GET
+    @Path("name/{name}")
+    public Response getCategoryByName(@PathParam("name")String name) {
 
-        return categoryService.getCategoriesByName(name);
+        List<CategoryDto> categoryDtos = categoryService.getCategoriesByName(name);
+        GenericEntity entity = new GenericEntity<List<CategoryDto>>(categoryDtos) {
+        };
+        return Response.ok(entity).build();
     }
 
-    public long getFilmsCountInCategory(int id) {
-        return categoryService.getFilmsInCategory(id);
+    @GET
+    @Path("count/{id}")
+    public Response getFilmsCountInCategory(@PathParam("id")int id) {
+
+        return Response.ok("films count = "+categoryService.getFilmsInCategory(id)).build();
     }
 }
